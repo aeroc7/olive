9c757f62ea47e5be00d2e38c3c6f2d5c5fa46471
Bennett <aerozotac@outlook.com>
2022-05-09 18:12:06 -0700

    New structur for ffmpeg decoder

---
+++ app/codec/ffmpeg/decode/ffmpegdecoder.h
@@ -0,0 +1,67 @@
+/***
+
+  Olive - Non-Linear Video Editor
+  Copyright (C) 2022 Olive Team
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+***/
+
+#ifndef FFMPEGDECODER_H
+#define FFMPEGDECODER_H
+
+#include <codec/decoder.h>
+#include <codec/ffmpeg/ffmpegframepool.h>
+
+struct AVStream;
+
+namespace olive {
+/**
+ * @brief A Decoder derivative that wraps FFmpeg functions as on Olive decoder
+ */
+class FFmpegDecoder : public Decoder
+{
+  Q_OBJECT
+public:
+  FFmpegDecoder() = default;
+  FFmpegDecoder(const FFmpegDecoder &) = delete;
+  FFmpegDecoder(FFmpegDecoder &&) = delete;
+  FFmpegDecoder &operator=(const FFmpegDecoder &) = delete;
+  FFmpegDecoder &operator=(FFmpegDecoder &&) = delete;
+  ~FFmpegDecoder() = default;
+
+  virtual QString id() const override;
+
+  virtual bool SupportsVideo() override{return true;}
+  virtual bool SupportsAudio() override{return true;}
+
+  virtual FootageDescription Probe(const QString &filename, const QAtomicInt *cancelled) const override;
+
+protected:
+  virtual bool OpenInternal() override;
+  virtual FramePtr RetrieveVideoInternal(const rational &timecode, const RetrieveVideoParams& params, const QAtomicInt *cancelled) override;
+  virtual bool ConformAudioInternal(const QVector<QString>& filenames, const AudioParams &params, const QAtomicInt* cancelled) override;
+  virtual void CloseInternal() override;
+
+private:
+
+  constexpr VideoParams::Format GetNativePixelFormat(AVPixelFormat pix_fmt) noexcept;
+  constexpr int GetNativeChannelCount(AVPixelFormat pix_fmt) noexcept;
+  std::int64_t ValidateChannelLayout(const AVStream *stream) noexcept;
+  constexpr const char* GetInterlacingModeInFFmpeg(VideoParams::Interlacing interlacing) noexcept;
+};
+
+}
+
+#endif // FFMPEGDECODER_H
